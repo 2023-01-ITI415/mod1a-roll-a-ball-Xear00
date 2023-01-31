5202e1fc128fce4ff46299f6528e4e016eaf39d9
Xear <zzier42@gmail.com>
2023-01-30 20:20:05 -0500

    Added many main components of game
    Put together the play area, the pickup items, as well as the UI and victory image.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+
     private Rigidbody rb;
+    private int count;
     public float movementX;
     public float movementY;
+
+    public GameObject winImageObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
+        count = 0;    
+
+        SetCountText();
+        winImageObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,10 +34,28 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if(count>=18){
+        winImageObject.SetActive(true);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+        other.gameObject.SetActive(false);
+        count++;
+        SetCountText();
+        }
     }
 }
